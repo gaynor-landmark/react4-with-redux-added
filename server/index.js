a9396d3301f7ddef92d9b7e8b3f8da2f3c34ea90
gaynor-landmark <g.l.landmark@gmail.com>
2016-04-13 10:36:06 +1200

    getting ready for heroku

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ app.use(require('body-parser').urlencoded({ extended: true }))
 if (require.main === module) {
   var port = process.env.PORT || 3000
   server.listen(port, function () {
-    console.log('Server is running on port 3000!')
+    console.log('Server is running on port ' + port)
   })
 }
